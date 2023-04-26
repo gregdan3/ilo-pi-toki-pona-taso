06957699a550aa8c2beaa099fb5bad67e011f064
Gregory Danielson <gregory.danielson3@gmail.com>
2023-04-26 17:57:31 -0500

    poki li wan la o pona tawa wan lon ilo lawa

--- src/tenpo/cogs/rules/cog.py
+++ src/tenpo/cogs/rules/cog.py
@@ -20,7 +20,7 @@ from discord.ext import commands
 from discord.commands.context import ApplicationContext
 
 # LOCAL
-from tenpo.db import Owner, Action, Container
+from tenpo.db import Action, Container
 from tenpo.__main__ import DB, TenpoBot
 from tenpo.log_utils import getLogger
 from tenpo.chat_utils import (
@@ -38,7 +38,7 @@ LOG = getLogger()
 # can't use discord.guild.GuildChannel because it includes categories
 MessageableGuildChannel = TextChannel | ForumChannel | StageChannel | VoiceChannel
 DiscordContainer = Guild | CategoryChannel | MessageableGuildChannel
-
+DiscordActor = Member | User | Guild
 
 
 # TODO:: generate these functions
@@ -50,12 +50,16 @@ class CogRules(Cog):
 
     @guild_rules.command(name="sona", description="ilo ni li ken seme? o pana e sona")
     async def guild_help(self, ctx: ApplicationContext):
-        return lawa_help(ctx)
+        actor = ctx.guild
+        assert actor
+        await cmd_lawa_help(ctx, actor, ephemeral=False)
 
     @guild_rules.command(name="ale", description="o lukin e lawa ma")
     @commands.has_permissions(administrator=True)
     async def guild_list_rules(self, ctx: ApplicationContext):
-        await list_rules(ctx, Owner.GUILD)
+        actor = ctx.guild
+        assert actor
+        await cmd_list_rules(ctx, actor, ephemeral=False)
 
     @guild_rules.command(name="tomo", description="o ante e lawa tomo")
     @option(name="tomo", description="lon tomo seme")
@@ -67,7 +71,9 @@ class CogRules(Cog):
         tomo: MessageableGuildChannel,
         ala: bool = False,
     ):
-        await cmd_toggle_rule(ctx, tomo, Container.CHANNEL, Owner.GUILD, ala)
+        actor = ctx.guild
+        assert actor
+        await cmd_toggle_rule(ctx, actor, tomo, Container.CHANNEL, ala, ephemeral=False)
 
     @guild_rules.command(name="kulupu", description="o ante e lawa kulupu")
     @option(name="kulupu", description="lon kulupu seme")
@@ -79,7 +85,11 @@ class CogRules(Cog):
         kulupu: MessageableGuildChannel,
         ala: bool = False,
     ):
-        await cmd_toggle_rule(ctx, kulupu, Container.CATEGORY, Owner.GUILD, ala)
+        actor = ctx.guild
+        assert actor
+        await cmd_toggle_rule(
+            ctx, actor, kulupu, Container.CATEGORY, ala, ephemeral=False
+        )
 
     @guild_rules.command(name="ma", description="o ante e lawa ma")
     @commands.has_permissions(administrator=True)
@@ -87,7 +97,9 @@ class CogRules(Cog):
         self,
         ctx: ApplicationContext,
     ):
-        await cmd_toggle_rule(ctx, ctx.guild, Container.GUILD, Owner.GUILD)
+        actor = ctx.guild
+        assert actor  # special case: server has a server rule
+        await cmd_toggle_rule(ctx, actor, actor, Container.GUILD, ephemeral=False)
 
     """
     User rules. They're nearly identical to guild ones.
@@ -96,11 +108,15 @@ class CogRules(Cog):
 
     @user_rules.command(name="sona", description="ilo ni li ken seme? o pana e sona")
     async def user_help(self, ctx: ApplicationContext):
-        return lawa_help(ctx)
+        actor = ctx.user
+        assert actor
+        await cmd_lawa_help(ctx, actor, ephemeral=True)
 
     @user_rules.command(name="ale", description="o lukin e lawa sina")
     async def user_list_rules(self, ctx: ApplicationContext):
-        await list_rules(ctx, Owner.USER)
+        actor = ctx.user
+        assert actor
+        await cmd_list_rules(ctx, actor, ephemeral=True)
 
     @user_rules.command(name="tomo", description="o ante e lawa tomo")
     @option(name="tomo", description="lon tomo seme")
@@ -111,7 +127,9 @@ class CogRules(Cog):
         tomo: MessageableGuildChannel,
         ala: bool = False,
     ):
-        await cmd_toggle_rule(ctx, tomo, Container.CHANNEL, Owner.USER, ala)
+        actor = ctx.user
+        assert actor
+        await cmd_toggle_rule(ctx, actor, tomo, Container.CHANNEL, ala, ephemeral=True)
 
     @user_rules.command(name="kulupu", description="o ante e lawa kulupu")
     @option(name="kulupu", description="lon kulupu seme")
@@ -122,14 +140,20 @@ class CogRules(Cog):
         kulupu: CategoryChannel,
         ala: bool = False,
     ):
-        await cmd_toggle_rule(ctx, kulupu, Container.CATEGORY, Owner.USER, ala)
+        actor = ctx.user
+        assert actor
+        await cmd_toggle_rule(
+            ctx, actor, kulupu, Container.CATEGORY, ala, ephemeral=True
+        )
 
     @user_rules.command(name="ma", description="o ante e lawa ma")
     async def user_toggle_guild(
         self,
         ctx: ApplicationContext,
     ):
-        await cmd_toggle_rule(ctx, ctx.guild, Container.GUILD, Owner.USER)
+        actor = ctx.user
+        assert actor
+        await cmd_toggle_rule(ctx, actor, ctx.guild, Container.GUILD, ephemeral=True)
 
     # user_open = user_rules.create_subgroup(
     #     name="open", description="mi o lukin ala e toki pi open seme"
@@ -150,11 +174,12 @@ class CogRules(Cog):
     )
     @option(name="sitelen", description="sitelen")
     async def user_manage_reacts(self, ctx: ApplicationContext, sitelen: str = ""):
+        # TODO: split in case guild may want to configure later
         user = ctx.user
         assert user
 
         if not sitelen:
-            await DB.set_reacts(user.id, Owner.USER, [])
+            await DB.set_reacts(user.id, [])
             await ctx.respond("sina pana e sitelen ala la mi weka e sitelen ale")
             return
 
@@ -166,11 +191,7 @@ class CogRules(Cog):
                 "sina pana e sitelen, taso mi sona ala e ona. ona li pona ala pona?"
             )
             return
-        await DB.set_reacts(user.id, Owner.USER, all_reacts)
-
-        if __debug__:
-            reacts = await DB.get_reacts(user.id, Owner.USER)
-            LOG.debug(reacts)
+        await DB.set_reacts(user.id, all_reacts)
 
         formatted_reacts = format_reacts(all_reacts)
         await ctx.respond(
@@ -179,41 +200,27 @@ class CogRules(Cog):
         )
 
 
-def owner_resp_coalesce(
-    otype: Owner, user: User | Member, guild: Guild
-) -> Tuple[User | Member | Guild, bool]:
-    if otype == Owner.USER:
-        owner = user
-        ephemeral = True
-    elif otype == Owner.GUILD:
-        owner = guild
-        ephemeral = False
-    return owner, ephemeral
-
-
 async def cmd_toggle_rule(
     ctx: ApplicationContext,
+    actor: DiscordActor,
     container: DiscordContainer,
     ctype: Container,
-    otype: Owner,  # owner object isn't needed if we have context and otype
     exception: bool = False,
+    ephemeral: bool = False,
 ):
     guild = ctx.guild
     assert guild
     user = ctx.user
     assert user
 
-    owner, ephemeral = owner_resp_coalesce(otype, user, guild)
-
-    action = await DB.toggle_rule(container.id, ctype, owner.id, otype, exception)
-    response = await build_rule_resp(container, ctype, otype, action, exception)
+    action = await DB.toggle_rule(container.id, ctype, actor.id, exception)
+    response = await build_rule_resp(container, ctype, action, exception)
     await ctx.respond(response, ephemeral=ephemeral)
 
 
 async def build_rule_resp(
     container: DiscordContainer,
     ctype: Container,
-    otype: Owner,
     action: Action,
     exception: bool,
 ):
@@ -237,42 +244,44 @@ async def build_rule_resp(
     return response
 
 
-async def list_rules(ctx: ApplicationContext, otype: Owner):
+async def cmd_list_rules(
+    ctx: ApplicationContext,
+    actor: DiscordActor,
+    ephemeral: bool,
+):
     guild = ctx.guild
     assert guild
     user = ctx.user
     assert user
 
-    owner, ephemeral = owner_resp_coalesce(otype, user, guild)
-
-    rules, exceptions = await DB.list_rules(owner.id, otype)
+    rules_info = ""
+    rules, exceptions = await DB.list_rules(actor.id)
     rules_info = format_rules_exceptions(rules, exceptions)
 
-    reacts = await DB.get_reacts(owner.id, otype)
-    reacts_info = format_reacts_rules(reacts)
+    reacts_info = ""
+    if isinstance(actor, Guild):
+        reacts = await DB.get_reacts(actor.id)
+        reacts_info = format_reacts_rules(reacts)
 
     result = rules_info + "\n\n" + reacts_info
 
     await ctx.respond(result, ephemeral=ephemeral)
 
 
-async def lawa_help(ctx: ApplicationContext):
-    sona = """mi __ilo pi toki pona taso__. sina toki pona ala la mi pana e sona pakala.
-/lawa sona: mi pana e toki ni.
+async def cmd_lawa_help(
+    ctx: ApplicationContext, actor: DiscordActor, ephemeral: bool = True
+):
+    sona = """mi __ilo pi toki pona taso__. sina toki pona ala la mi pona e ona. o lukin e ken mi:
 
-lawa la mi lukin pona e toki sina.
-mi ken ni lon **tomo** lon **kulupu** tomo lon **ma**.
-
-sina lawa wan e ijo la mi lukin e ona.
-sina lawa tu e ijo sama la mi weka e lukin.
-
-/lawa ale: o lukin e lawa ale sina.
-/lawa [tomo|kulupu|ma] (ala): mi lukin e ijo.
-    - ala la mi lukin ala e ijo. ni li ken lon kulupu pi toki pona lon ma pi toki pona.
-    - sina lawa tu e ijo sama la mi weka e lukin.
-
-/lawa open [toki]: toki li lon open pi toki sina la mi lukin ala e ona.
-/lawa sitelen [sitelen]: sina toki pona ala la mi pana e sitelen.
-/lawa weka [True|False]: sina toki pona ala la mi weka e toki. sina wile ala e weka la
+`/lawa sona`: mi pana e toki ni.
+`/lawa ale`: mi pana e lawa ale sina.
+`/lawa [tomo|kulupu|ma] (ala)`: mi lukin e ijo.
+    - sina toki pona ala lon ijo la mi pona e ona.
+    - `ala` la mi lukin ala e ijo. ijo ni li ken lon insa pi ijo pi toki pona.
+    - sina pana tu e ijo sama la mi weka e lukin.
+`/lawa open [toki]`: toki li lon open pi toki sina la mi lukin ala e ona.
+    - sina pana tu e toki sama la mi weka e ona.
+`/lawa sitelen [sitelen]`: sina toki pona ala la mi pana e sitelen.
+`/lawa weka [True|False]`: sina toki pona ala la mi weka e toki. sina wile ala e weka la
     """
-    await ctx.respond(sona)
+    await ctx.respond(sona, ephemeral=ephemeral)
