cfcfc0ce53bfaf5b90e2bbcbc3f0ec9e2c1cce4e
Gregory Danielson <gregory.danielson3@gmail.com>
2023-08-21 20:21:03 -0500

    lili la o pona

--- src/tenpo/cogs/phase-calendar/cog.py
+++ src/tenpo/cogs/phase-calendar/cog.py
@@ -2,8 +2,9 @@
 from typing import cast
 
 # PDM
-from discord import Bot, Cog, ApplicationContext
-from discord.ext import tasks, commands
+import discord
+from discord import Bot, Cog
+from discord.ext import tasks
 
 # LOCAL
 from tenpo.types import MessageableGuildChannel
@@ -14,6 +15,16 @@ from tenpo.phase_utils import current_emoji, is_major_phase
 LOG = getLogger()
 
 
+def get_calendar_title():
+    # TODO: nimi li ken ante
+    title = "mun tenpo"
+    if is_major_phase():
+        title = "o toki pona taso"
+    phase_emoji = current_emoji()
+    title = f"{phase_emoji} {title}"
+    return title
+
+
 class CogPhaseCalendar(Cog):
     def __init__(self, bot: Bot):
         self.bot: Bot = bot
@@ -21,16 +32,11 @@ class CogPhaseCalendar(Cog):
 
     @tasks.loop(minutes=30)
     async def moon_calendar(self):
-        LOG.info(type(DB))
-        LOG.info("Checking moon calendar")
-        title = "mun tenpo"
-        if is_major_phase():
-            title = "o toki pona taso"
-        phase_emoji = current_emoji()
-        title = f"{phase_emoji} {title}"
+        title = get_calendar_title()
 
         moon_channels = await DB.get_calendars()
         LOG.debug(moon_channels)
+
         for channel_id in moon_channels:
             assert isinstance(channel_id, int)
             channel = cast(MessageableGuildChannel, self.bot.get_channel(channel_id))
